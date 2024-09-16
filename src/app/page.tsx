1e2b2d22089efecc5323a733ddeb560279158d00
clumsy-ug <rutobe740@gmail.com>
2024-09-16 14:39:11 +0900

    homepage実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,17 @@
-import SignOutButton from "@/components/SignOutButton";
-import { createClient } from "@/supabase/server";
-import { Session } from "@supabase/supabase-js";
+import Link from "next/link";
 
-const Home = async () => {
-    const supabase = createClient();
-
-    /* useStateなどのclient系使えない、しかしsessionはグローバルで保持したい、そしてエラーハンドリングもしたい
-    -> グローバル変数としてsessionを保存することで解決 */
-    let globalSession: Session | null = null;
-
-    try {
-        const { data: { session } } = await supabase.auth.getSession();
-        if (session) globalSession = session;
-    } catch (e) {
-        console.error('Home内のエラー->', e);
-    }
-
+const Home = () => {
     return (
-        <div>
+        <>
             <h1>Welcome to the home page</h1>
-            {globalSession ? (
-                <>
-                    <p>
-                        You are logged in as <b>{globalSession.user.email}</b>
-                    </p>
-                    <SignOutButton />
-                </>
-            ) : (
-                <p>You are not logged in</p>
-            )}
-        </div>
+            <p>自分だけのTodoリストを作成しよう！</p>
+            <p>各Todoには自由なタグをつけることができます。</p>
+            <p>タグでTodoを検索することができます。</p>
+
+            <br />
+
+            <Link href="/login">はじめる</Link>
+        </>
     );
 };
 
