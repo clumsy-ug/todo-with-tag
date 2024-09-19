55c36dead6878dade967d2595f7ddbb66f3b06dd
clumsy-ug <rutobe740@gmail.com>
2024-09-19 19:03:10 +0900

    deleteTodo実装

---
+++ src/supabase/CRUD/deleteTodo.ts
@@ -0,0 +1,24 @@
+import { createClient } from "../client";
+
+const deleteTodo = async (todoId: string): Promise<boolean> => {
+    const supabase = createClient();
+
+    try {
+        const { error } = await supabase
+            .from('todos')
+            .delete()
+            .eq('id', todoId);
+
+        if (error) {
+            console.error('deleteTodo内のerror->', error);
+            return false;
+        }
+
+        return true;
+    } catch (e) {
+        console.error('deleteTodo内のe->', e);
+        return false;
+    }
+}
+
+export default deleteTodo;
