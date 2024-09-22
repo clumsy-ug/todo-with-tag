f914b92b02402ed41e3445aaaeaeb24079c102b2
clumsy-ug <rutobe740@gmail.com>
2024-09-22 19:09:49 +0900

    不要なコメントを削除

--- src/supabase/CRUD/insertTag.ts
+++ src/supabase/CRUD/insertTag.ts
@@ -1,10 +1,3 @@
-/*
-front側でtags内をmapでまわして、各要素に対して以下のinsertTagを実行しよう
-そうすれば各tagを配列ではなく文字列としてそれぞれtagsテーブルに登録できるので
-で、その際に、tagを1つも入力しなかったらそもそもinsertTagを実行しないようにしよう
-じゃないとそもそもnameを入力できずエラーになると思うので
-*/
-
 import { createClient } from "../client";
 
 const insertTag = async (id: string, name: string): Promise<boolean> => {
