5ad8134e6f42eebf671a4078d567982677458878
clumsy-ug <rutobe740@gmail.com>
2024-09-23 23:28:39 +0900

    エラーページ修正

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 const ErrorPage = () => {
-    return <p>Sorry, something went wrong</p>;
+    return (
+        <>
+            <p>Sorry, something went wrong</p>
+            <p>申し訳ございません、何か問題が発生しました</p>
+            <Link href='/login'>ログインページへ</Link>
+        </>
+    )
 }
 
 export default ErrorPage;
