6d28bc98695d2eaa7922bde2efdff1214e8ea360
clumsy-ug <rutobe740@gmail.com>
2024-09-26 19:50:24 +0900

    login-okのスタイリング

--- src/app/login-ok/page.tsx
+++ src/app/login-ok/page.tsx
@@ -3,14 +3,25 @@ import { createClient } from "@/supabase/server";
 
 const LoginOk = async () => {
     const supabase = createClient();
+
     const { data: { user } } = await supabase.auth.getUser();
     const userId = user?.id;
 
     return (
-        <>
-            <h3>ログイン完了！</h3>
-            <Link href={`todos/${userId}`} scroll={false}>あなたのtodoへ</Link>
-        </>
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <div className="p-8 rounded-lg shadow-md text-center w-72">
+                <p className="text-green-600 text-2xl mb-4 font-bold">
+                    ログイン完了！
+                </p>
+                <Link
+                    href={`todos/${userId}`}
+                    scroll={false}
+                    className="text-blue-600 underline hover:text-blue-800"
+                >
+                    あなたのtodoへ
+                </Link>
+            </div>
+        </div>
     )
 }
 
