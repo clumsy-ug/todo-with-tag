23846fc0e18b6b9f301de13eac93e29b236ab4a9
clumsy-ug <rutobe740@gmail.com>
2024-09-26 19:47:41 +0900

    不要なbgスタイルを削除

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const ErrorPage = () => {
     return (
         <div className="flex justify-center items-center min-h-screen bg-gray-100">
-            <div className="bg-white p-8 rounded-lg shadow-md text-center">
+            <div className="p-8 rounded-lg shadow-md text-center">
                 <p className="text-xl font-semibold text-red-600 mb-4">
                     申し訳ございません、問題が発生しました
                 </p>
