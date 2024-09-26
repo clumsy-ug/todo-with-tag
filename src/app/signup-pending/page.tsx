b49b081b0cf9f399c4034f3237f4ffb23a714698
clumsy-ug <rutobe740@gmail.com>
2024-09-26 20:18:53 +0900

    signup-pendingスタイリング

--- src/app/signup-pending/page.tsx
+++ src/app/signup-pending/page.tsx
@@ -1,6 +1,10 @@
 const SignupPending = () => {
     return (
-        <h3>メールアドレスに認証メールを送信しました。確認してください。</h3>
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <div className="shadow-md p-8 text-center rounded-lg">
+                <p className="text-blue-600 font-bold text-lg">メールアドレスに認証メールを送信しました✅ 確認してください。</p>
+            </div>
+        </div>
     )
 }
 
