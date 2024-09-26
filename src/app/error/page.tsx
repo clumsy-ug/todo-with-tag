2a9169b4b0df278879ddd7adb5db40d77adea875
clumsy-ug <rutobe740@gmail.com>
2024-09-26 19:54:50 +0900

    boldもsemibold変わらないので文字数小さいboldに変更

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -4,7 +4,7 @@ const ErrorPage = () => {
     return (
         <div className="flex justify-center items-center min-h-screen bg-gray-100">
             <div className="p-8 rounded-lg shadow-md text-center">
-                <p className="text-xl font-semibold text-red-600 mb-4">
+                <p className="text-xl font-bold text-red-600 mb-4">
                     申し訳ございません、問題が発生しました
                 </p>
                 <Link
