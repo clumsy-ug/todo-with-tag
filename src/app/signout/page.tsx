5ba9da092e2b910d6ebf121d1ff211384df449df
clumsy-ug <rutobe740@gmail.com>
2024-09-26 20:12:34 +0900

    signoutスタイリング

--- src/app/signout/page.tsx
+++ src/app/signout/page.tsx
@@ -1,7 +1,14 @@
 const SignOut = () => {
     return (
-        <form action="/auth/signout" method="post">
-            <button type="submit">
+        <form
+            action="/auth/signout"
+            method="post"
+            className="flex justify-center items-center min-h-screen"
+        >
+            <button
+                type="submit"
+                className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded duration-300 hover:scale-110"
+            >
                 Sign out
             </button>
         </form>
