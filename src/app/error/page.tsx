d428768b3e15b679834b41182c3734990c6df7a3
clumsy-ug <rutobe740@gmail.com>
2024-09-26 19:16:02 +0900

    不要なパディング削除

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const ErrorPage = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
             <div className="bg-white p-8 rounded-lg shadow-md text-center">
                 <p className="text-xl font-semibold text-red-600 mb-4">
                     申し訳ございません、問題が発生しました
