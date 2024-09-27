ecb9b79dfa13c81858231ef767107690fef09ded
clumsy-ug <rutobe740@gmail.com>
2024-09-27 13:01:28 +0900

    不要なcomponentを削除

--- src/components/SignOutButton.tsx
+++
@@ -1,9 +0,0 @@
-import { signout } from "@/app/login/actions";
-
-export default function SignOutButton() {
-    return (
-        <form>
-            <button formAction={signout}>Sign out</button>
-        </form>
-    );
-}
