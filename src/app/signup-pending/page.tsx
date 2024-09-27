d58b09e52526692dbb3d1be0bf74dd422e06483d
clumsy-ug <rutobe740@gmail.com>
2024-09-27 15:10:48 +0900

    誘導の文言をわかりやすく

--- src/app/signup-pending/page.tsx
+++ src/app/signup-pending/page.tsx
@@ -3,6 +3,7 @@ const SignupPending = () => {
         <div className="flex justify-center items-center min-h-screen bg-gray-100">
             <div className="shadow-md p-8 text-center rounded-lg">
                 <p className="text-blue-600 font-bold text-lg">メールアドレスに認証メールを送信しました✅ 確認してください。</p>
+                <p>(このタブは閉じて構いません)</p>
             </div>
         </div>
     )
